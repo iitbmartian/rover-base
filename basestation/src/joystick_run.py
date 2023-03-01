02b6dfd6edbbdf192c1197e035bded25a4e075ca
Vedika <vedika111103@gmail.com>
2023-03-02 00:17:57 +0530

    Arm servo joystick fixes

--- basestation/src/joystick_run.py
+++ basestation/src/joystick_run.py
@@ -59,10 +59,10 @@ def joy_callback(joy_inp):
         arm_out.shoulder_actuator.speed = 0
 
     # Elbow Actuator
-    if joy_inp_axes[7] > 25/120:
+    if joy_inp_axes[7] < -25/120:
         arm_out.elbow_actuator.direction = "forward"
         arm_out.elbow_actuator.speed = int(abs(joy_inp_axes[7]*120))
-    elif joy_inp_axes[7] < -25/120:
+    elif joy_inp_axes[7] > 25/120:
         arm_out.elbow_actuator.direction = "backward"
         arm_out.elbow_actuator.speed = int(abs(joy_inp_axes[7] * 120))
     else:
@@ -70,10 +70,10 @@ def joy_callback(joy_inp):
         arm_out.elbow_actuator.speed = 0
 
     # Base Motors
-    if joy_inp_axes[3] > 25/120:
+    if joy_inp_axes[3] < -25/120:
         arm_out.base_motor.direction = "forward"
         arm_out.base_motor.speed = int(abs(joy_inp_axes[3]*120))
-    elif joy_inp_axes[3] < -25/120:
+    elif joy_inp_axes[3] > 25/120:
         arm_out.base_motor.direction = "backward"
         arm_out.base_motor.speed = int(abs(joy_inp_axes[3]*120))
     else:
@@ -89,10 +89,10 @@ def joy_callback(joy_inp):
         arm_out.base_motor.speed = 120
 
     # Gripper
-    if joy_inp_buttons[1] == 1:
+    if joy_inp_buttons[2] == 1:
         arm_out.gripper.direction = "forward"
         arm_out.gripper.speed = 120
-    elif joy_inp_buttons[2] == 1:
+    elif joy_inp_buttons[1] == 1:
         arm_out.gripper.direction = "backward"
         arm_out.gripper.speed = 120
     else:
@@ -111,10 +111,10 @@ def joy_callback(joy_inp):
         arm_out.wrist_actuator.speed = 0
 
     # Gripper Rotation
-    if joy_inp_buttons[5] == 1:
+    if joy_inp_buttons[4] == 1:
         arm_out.gripper_rot.direction = "forward"
         arm_out.gripper_rot.speed = 120
-    elif joy_inp_buttons[4] == 1:
+    elif joy_inp_buttons[5] == 1:
         arm_out.gripper_rot.direction = "backward"
         arm_out.gripper_rot.speed = 120
     else:
@@ -122,10 +122,10 @@ def joy_callback(joy_inp):
         arm_out.gripper_rot.speed = 0
 
     # Elbow Motor
-    if joy_inp_axes[2] < 0:
+    if joy_inp_axes[5] < 0:
         arm_out.elbow_motor.direction = "forward"
         arm_out.elbow_motor.speed = int(abs(joy_inp_axes[2]*120))
-    elif joy_inp_axes[5] < 0:
+    elif joy_inp_axes[2] < 0:
         arm_out.elbow_motor.direction = "backward"
         arm_out.elbow_motor.speed = int(abs(joy_inp_axes[5]*120))
     else:
