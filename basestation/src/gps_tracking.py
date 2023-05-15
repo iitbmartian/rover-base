2bba477d866b92d811a978e06994aee156f48bdc
Vedika Rathi <vedika111103@gmail.com>
2023-05-15 12:35:04 +0530

    gps_tracking modified to have multiple locations

--- basestation/src/gps_tracking.py
+++ basestation/src/gps_tracking.py
@@ -7,29 +7,41 @@ from time import sleep
 import rospy
 from std_msgs.msg import Float64MultiArray
 
+global location
+
 def gps_callback(inp):
-    plt.xlim(0,500)
-    plt.ylim(0,500)
+    plt.xlim(0,200)
+    plt.ylim(0,200)
     x = [0,0]
     y = [0,0]
     point = plt.plot(x,y,'ro')[0]
     plt.ion()
-    plt.show()
-    rover_x = inp.data[0]
-    rover_y = inp.data[1]
-    print(rover_x,rover_y)
-    x = [rover_x,gps_x]
-    y = [rover_y,gps_y]
+    rover_x = [inp.data[0]]
+    rover_y = [inp.data[1]]
+    x = gps_x + rover_x
+    y = gps_y + rover_y
     point.set_data(x,y)
+    for j in range(location):
+        plt.annotate('Loc'+str(j+1),(x[j],y[j]))
     print(point)
     plt.pause(1)
 
 if __name__=='__main__':
     rospy.init_node('GPS_Node')
     rospy.loginfo('Starting GPS Tracking Node')
-    gps_x = float(input('What is the x coordinate? '))
-    gps_y = float(input('What is the y coordinate? '))
+    location=0
+    gps_x = []
+    gps_y = []
+    while True:
+        exists = input('Is there a new Location? y/n: ')
+        if exists == 'y':
+            gps_x.append(float(input('What is the x coordinate? ')))
+            gps_y.append(float(input('What is the y coordinate ')))
+            location+=1
+        else:
+            break
     while not rospy.is_shutdown():
+        plt.show()
         rospy.Subscriber('/LatLon',Float64MultiArray,gps_callback)
         rospy.spin()
 
